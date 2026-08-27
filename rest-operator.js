8f7ae96906e51a6ce8b7c1b63cbe66de9093a34e
pgrmrmortuja <moutasimmortuja@gmail.com>
2026-08-27 23:05:51 +0600

    function rest operator

--- rest-operator.js
+++ rest-operator.js
@@ -20,3 +20,31 @@ const {id, ...others} = user;
 console.log(id);
 console.log(others);
 
+//function rest
+//1
+const total = (...numbers) => {
+    console.log(numbers) //[ 1, 2, 3 ]
+}
+
+total(1,2,3);
+
+//2
+const attendance = (captain, ...students) =>{
+    console.log(captain)
+    console.log(students);
+}
+
+attendance("Reyad" , "A", "B", "c")
+
+//3
+const add = (...nums) => {
+    let sum = 0;
+
+    for(num of nums){
+        sum += num;
+    }
+
+    console.log(sum); //12
+}
+add(3,4,5);
+
