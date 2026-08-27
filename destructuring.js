7f9ab071493b4a97f597103d13561c75620680ff
pgrmrmortuja <moutasimmortuja@gmail.com>
2026-08-27 20:49:56 +0600

    array destructuring

--- destructuring.js
+++ destructuring.js
@@ -8,3 +8,15 @@ const {name , age} = user;
 
 console.log(name, age);
 
+//array
+const arr = ["Reyad", 24];
+
+const [, ag] = arr;
+const [nam,] = arr;
+
+const [namee,agee] = arr;
+
+console.log(ag);
+console.log(nam);
+console.log(namee,agee);
+
