555008a1ba84c68ddbde368b693738f27d268bba
pgrmrmortuja <moutasimmortuja@gmail.com>
2026-08-27 21:09:21 +0600

    object spread

--- spread-operator.js
+++ spread-operator.js
@@ -7,3 +7,12 @@ const combinedArr = [...arr1 , ...arr2, ...arr3];
 console.log(arr3)
 console.log(combinedArr)
 
+
+//object spread
+const obj1 = {name: "Reyad", age: 25};
+const obj2 = {city: "Dhaka", country: "BD"};
+const obj3 = {...obj1, ...obj2, height: 5};
+const mergedObj = {...obj1, ...obj2, ...obj3};
+
+console.log(obj3)
+console.log(mergedObj)
