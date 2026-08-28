c94937b1d73a462166c52f3460432b6949e918f6
pgrmrmortuja <moutasimmortuja@gmail.com>
2026-08-28 14:24:36 +0600

    string sort

--- array-method-02.js
+++ array-method-02.js
@@ -12,3 +12,16 @@ const sum = numbers.reduce((acc, n) => {
 console.log(sum);
 
 
+//sort
+//string sort
+const str = ["Apple", "Mango", "Banana"];
+const sortStr = str.sort();
+console.log(sortStr)
+
+
+//number sort
+const numbs = [3,6,7,2,8,1];
+const sortCapital = numbs.sort((a,b) => a-b);
+const sortSmall = numbs.sort((a,b) => b - a);
+console.log(sortCapital);
+console.log(sortSmall);
